210038e29071b089979f421ca4ea7859de2c4822
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-21 10:02:05 -0700

    add dotenv to module plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin'); 
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -22,7 +24,8 @@ module.exports = {
       title: 'Shape Tracker',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
