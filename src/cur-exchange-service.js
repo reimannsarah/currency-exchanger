13b943820e2fb7d229b8e8ac4742f55bf43c9835
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-21 08:45:32 -0700

    add Exchange class with statis method and error handling

---
+++ src/cur-exchange-service.js
@@ -0,0 +1,15 @@
+export default class Exchange {
+  static async getNewCurrency(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+      .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        return error;
+      });
+  };
+};
