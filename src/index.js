5bce7a16cf8bfecb2c39267cb41dbc919adcb0e1
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-21 08:59:53 -0700

    add exchange currency function to call API and handles errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './cur-exchange-service';
+
+function exchangeCurrency(currency) {
+  Exchange.getNewCurrency(currency)
+    .then(response => {
+      if (response instanceof Error) {
+        const errorMessage = `This currency does not exist:
+        ${response.message}`;
+        throw new Error(errorMessage);
+      }
+      printExchangedCurrency();
+    })
+    .catch(error => {
+      printError(error);
+    });
+};
